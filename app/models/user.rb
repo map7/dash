86addb5de518436bb3423ab44817b3f85a2da896
Michael Pope <michael@dtcorp.com.au>
2025-09-30 11:26:11 +1000

    User-FEAT-Link users to apps via accesses
    EG: User accesses many apps

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  has_many :accesses
+  has_many :apps, :through => :accesses
+  
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
