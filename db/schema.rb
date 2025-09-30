9c027127e27ad264fd69ad31a61ff6d2dde39c26
Michael Pope <michael@dtcorp.com.au>
2025-09-30 11:20:19 +1000

    DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_30_001304) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_30_011804) do
+  create_table "accesses", force: :cascade do |t|
+    t.integer "menu_position"
+    t.string "login"
+    t.string "password"
+    t.string "auto_login"
+    t.integer "user_id", null: false
+    t.integer "app_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["app_id"], name: "index_accesses_on_app_id"
+    t.index ["user_id"], name: "index_accesses_on_user_id"
+  end
+
   create_table "apps", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -38,5 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_30_001304) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "accesses", "apps"
+  add_foreign_key "accesses", "users"
   add_foreign_key "sessions", "users"
 end
