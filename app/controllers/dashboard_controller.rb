11692afae0c17aa51932ced9ca1c62229ac834bb
Michael Pope <michael@dtcorp.com.au>
2025-10-07 13:54:01 +1100

    Access-FEAT-Check if password is nil or length zero

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,13 +8,13 @@ class DashboardController < ApplicationController
 
     access = Current.user.access_for(app)
 
-    if access.password.length > 0
-      # If we have a password then go to the website
-      redirect_to app.url, allow_other_host: true
-
-    else
+    if access.password.nil? or access.password.length == 0
       # Edit the password
       redirect_to edit_access_path(access)
+
+    else
+      # If we have a password then go to the website
+      redirect_to app.url, allow_other_host: true
     end    
   end
 end
