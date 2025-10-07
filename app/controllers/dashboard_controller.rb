b6006069bfb5f37971cbf754ab110e5cec23c84f
Michael Pope <michael@dtcorp.com.au>
2025-10-07 11:37:01 +1100

    Access-FEAT-Action the URL link through the controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,9 @@ class DashboardController < ApplicationController
   def index
     @apps = Current.user.apps
   end
+
+  def show
+    app = App.find(params[:id])
+    redirect_to app.url, allow_other_host: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resource :registration, only: %i[new create]
+  resources :dashboard, only: %i[index show]
   
   get "dashboard/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
