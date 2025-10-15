a317687dbb099513f7df00a0aa0854c9ab841938
Michael Pope <michael@dtcorp.com.au>
2025-10-15 12:13:29 +1100

    Dash-FEAT-Prepare show page for auto login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,10 +7,10 @@ class DashboardController < ApplicationController
     app = App.find(params[:id])
     access = Current.user.access_for(app)
 
-    login = access.login.to_s.length == 0 ? app.global_login : access.login
-    password = access.password.to_s.length == 0 ? app.global_password : access.password
+    @login = access.login.to_s.length == 0 ? app.global_login : access.login
+    @password = access.password.to_s.length == 0 ? app.global_password : access.password
 
-    if (password.nil? or password.length == 0) and access.auto_login != 'None'
+    if (@password.nil? or @password.length == 0) and access.auto_login != 'None'
     
       # Edit the password
       redirect_to edit_access_path(access)
@@ -18,10 +18,14 @@ class DashboardController < ApplicationController
     else
       # If we have a password then go to the website
       if access.auto_login == 'Basic Auth'
-        url = app.url.split('//').insert(1, "//#{login}:#{password}@").join()
+        url = app.url.split('//').insert(1, "//#{@login}:#{@password}@").join()
         redirect_to url, allow_other_host: true
       else
-        redirect_to app.url, allow_other_host: true
+        @url = app.url
+        @auto_login = access.auto_login
+        
+        render :show
+        # redirect_to app.url, allow_other_host: true
       end
     end    
   end
