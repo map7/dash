bdb7aea05df476e99cdae0b2143c5b6317320a26
Michael Pope <map7777@gmail.com>
2025-10-24 11:57:34 +1100

    TEST-Create a helper to login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,19 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+
+    def login
+      get "/session/new"
+      assert_dom "h1", "Sign in"
+
+      # post the login and follow through to the home page
+      post session_path, params: { email_address: users(:one).email_address,
+                                  password: "password" }
+
+      follow_redirect!
+      assert_equal 200, status
+    end
+
   end
+
 end
