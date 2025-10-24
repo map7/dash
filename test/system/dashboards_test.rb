b2d1cf9011e583fd7aac67be904ce6cb5b734a90
Michael Pope <map7777@gmail.com>
2025-10-24 12:25:05 +1100

    TEST-Update dashboard system test

--- test/system/dashboards_test.rb
+++ test/system/dashboards_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class DashboardsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit dashboards_url
-  #
-  #   assert_selector "h1", text: "Dashboard"
-  # end
+  test "visiting the index" do
+    visit dashboard_index_url
+  
+    assert_selector "h1", text: "Sign in"
+    
+  end
 end
