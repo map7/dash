af9cd0472a216b20d5223b9c9dfcef99107f406e
Michael Pope <map7777@gmail.com>
2025-10-24 11:41:45 +1100

    TEST-Add login integration test

---
+++ test/integration/login_flow_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class LoginFlowTest < ActionDispatch::IntegrationTest
+  test "logs in and is presented with the dashboard" do
+    get "/session/new"
+    assert_dom "h1", "Sign in"
+
+    # post the login and follow through to the home page
+    post session_path, params: { email_address: users(:one).email_address,
+                                password: "password" }
+
+    follow_redirect!
+    assert_equal 200, status
+    assert_equal "/", path
+  end
+end
