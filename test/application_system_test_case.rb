44dfef6872d04a6ce87ae22ad37605a31af07e02
Michael Pope <map7777@gmail.com>
2025-10-24 14:42:10 +1100

    TEST-Extract out login test code

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,4 +3,13 @@ require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :cuprite_rails
+
+  def login_user
+    assert_selector "h1", text: "Sign in"
+
+    fill_in :email_address, with: users(:one).email_address
+    fill_in :password, with: "password"
+
+    click_on "Sign in"
+  end
 end
--- test/system/dashboards_test.rb
+++ test/system/dashboards_test.rb
@@ -1,36 +1,20 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class DashboardsTest < ApplicationSystemTestCase
-  test "Login and direct to the first app" do
-
+  test 'Login and direct to the first app' do
     visit dashboard_index_url
-  
-    assert_selector "h1", text: "Sign in"
+    login_user
+    assert_selector 'h1', text: 'Tramontana Dash ADD APP'
 
-    fill_in :email_address, with: users(:one).email_address
-    fill_in :password, with: "password"
-
-    click_on "Sign in"
-
-    assert_selector "h1", text: "Tramontana Dash ADD APP"
-    
     # page.driver.debug(binding)
-    click_link(id: '0')         # Google link
+    # Google link
+    click_link(id: '0')
   end
 
   test 'Login and direct to the second app and asks for password' do
     visit dashboard_index_url
-
-    assert_selector 'h1', text: 'Sign in'
-
-    fill_in :email_address, with: users(:one).email_address
-    fill_in :password, with: 'password'
-
-    click_on 'Sign in'
-
+    login_user
     assert_selector 'h1', text: 'Tramontana Dash ADD APP'
-
-    # page.driver.debug(binding)
 
     # Report Craft
     click_link(id: '1')
