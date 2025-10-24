487f3a99d616e58eabe91cd9635579e5d68553e7
Michael Pope <map7777@gmail.com>
2025-10-24 14:38:34 +1100

    TEST-Add test that it prompts for credentials

--- test/system/dashboards_test.rb
+++ test/system/dashboards_test.rb
@@ -17,4 +17,24 @@ class DashboardsTest < ApplicationSystemTestCase
     # page.driver.debug(binding)
     click_link(id: '0')         # Google link
   end
+
+  test 'Login and direct to the second app and asks for password' do
+    visit dashboard_index_url
+
+    assert_selector 'h1', text: 'Sign in'
+
+    fill_in :email_address, with: users(:one).email_address
+    fill_in :password, with: 'password'
+
+    click_on 'Sign in'
+
+    assert_selector 'h1', text: 'Tramontana Dash ADD APP'
+
+    # page.driver.debug(binding)
+
+    # Report Craft
+    click_link(id: '1')
+
+    assert_selector 'h1', text: 'Editing access'
+  end
 end
