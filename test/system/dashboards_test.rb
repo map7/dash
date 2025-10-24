87c0e4b8ed39ce12705f0b691bd22b657d1b13c1
Michael Pope <map7777@gmail.com>
2025-10-24 13:20:26 +1100

    TEST-System test to login to the dashboard

--- test/system/dashboards_test.rb
+++ test/system/dashboards_test.rb
@@ -5,6 +5,13 @@ class DashboardsTest < ApplicationSystemTestCase
     visit dashboard_index_url
   
     assert_selector "h1", text: "Sign in"
-    
+
+    fill_in :email_address, with: users(:one).email_address
+    fill_in :password, with: "password"
+
+    click_on "Sign in"
+    # page.driver.debug(binding)
+
+    assert_selector "h1", text: "Tramontana Dash ADD APP"
   end
 end
