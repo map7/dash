c04a78a7cb1c0a9737b6d43a9de30596a143b871
Michael Pope <map7777@gmail.com>
2025-10-24 14:49:41 +1100

    TEST-Test adding a new app

--- test/system/apps_test.rb
+++ test/system/apps_test.rb
@@ -2,28 +2,25 @@ require "application_system_test_case"
 
 class AppsTest < ApplicationSystemTestCase
   setup do
-    @app = apps(:one)
+    @app = apps(:three)
   end
 
-  # test "visiting the index" do
-  #   visit apps_url
-  #   assert_selector "h1", text: "Apps"
-  # end
+  test "should create app" do
+    visit dashboard_index_url
+    login_user
+    
+    click_on "ADD APP"
 
-  # test "should create app" do
-  #   visit apps_url
-  #   click_on "New app"
+    fill_in "Description", with: @app.description
+    fill_in "Global login", with: @app.global_login
+    fill_in "Global password", with: @app.global_password
+    fill_in "Name", with: @app.name
+    fill_in "Url", with: @app.url
+    click_on "Create App"
 
-  #   fill_in "Description", with: @app.description
-  #   fill_in "Global login", with: @app.global_login
-  #   fill_in "Global password", with: @app.global_password
-  #   fill_in "Name", with: @app.name
-  #   fill_in "Url", with: @app.url
-  #   click_on "Create App"
-
-  #   assert_text "App was successfully created"
-  #   click_on "Back"
-  # end
+    assert_selector 'h1', text: 'Tramontana Dash ADD APP'
+    assert_text "Feather WIKI"
+  end
 
   # test "should update App" do
   #   visit app_url(@app)
