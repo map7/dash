d03eb631689ac4c627032bd2771a64139d2ab0fe
Michael Pope <michael@dtcorp.com.au>
2025-10-27 10:04:28 +1100

    TEST-edit access

--- test/system/accesses_test.rb
+++ test/system/accesses_test.rb
@@ -5,47 +5,20 @@ class AccessesTest < ApplicationSystemTestCase
     @access = accesses(:one)
   end
 
-  # test "visiting the index" do
-  #   visit accesses_url
-  #   assert_selector "h1", text: "Accesses"
-  # end
+  test "should update Access" do
+    visit dashboard_index_url
+    login_user
 
-  # test "should create access" do
-  #   visit accesses_url
-  #   click_on "New access"
+    click_on "access2"
 
-  #   fill_in "App", with: @access.app_id
-  #   fill_in "Auto login", with: @access.auto_login
-  #   fill_in "Login", with: @access.login
-  #   fill_in "Menu position", with: @access.menu_position
-  #   fill_in "Password", with: @access.password
-  #   fill_in "User", with: @access.user_id
-  #   click_on "Create Access"
+    fill_in "access_login", with: "admin"
+    fill_in "access_password", with: "password"
+    select "Basic Auth", from: "access_auto_login"
 
-  #   assert_text "Access was successfully created"
-  #   click_on "Back"
-  # end
+    click_on "Update Access"
 
-  # test "should update Access" do
-  #   visit access_url(@access)
-  #   click_on "Edit this access", match: :first
+    # assert_text "App was successfully updated"
 
-  #   fill_in "App", with: @access.app_id
-  #   fill_in "Auto login", with: @access.auto_login
-  #   fill_in "Login", with: @access.login
-  #   fill_in "Menu position", with: @access.menu_position
-  #   fill_in "Password", with: @access.password
-  #   fill_in "User", with: @access.user_id
-  #   click_on "Update Access"
-
-  #   assert_text "Access was successfully updated"
-  #   click_on "Back"
-  # end
-
-  # test "should destroy Access" do
-  #   visit access_url(@access)
-  #   accept_confirm { click_on "Destroy this access", match: :first }
-
-  #   assert_text "Access was successfully destroyed"
-  # end
+    assert_selector 'h1', text: 'Tramontana Dash ADD APP'
+  end
 end
